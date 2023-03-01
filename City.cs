494bb5eab6a6bac67dbeb01db9be93ea64b6531c
Djan <124775935+DjanRamadan@users.noreply.github.com>
2023-03-01 13:02:06 +0200

    Add files via upload

---
+++ City.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter the name of the town: ");
+string name = Console.ReadLine();
+Console.Write("Enter the town's population: ");
+int population = int.Parse(Console.ReadLine());
+Console.Write("Enter the town's area: ");
+double area = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Town name: {name}");
+Console.WriteLine($"{name}'s population: {population}");
+Console.WriteLine($"{name}'s area: {area}");
